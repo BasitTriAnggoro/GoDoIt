999005321f7d605ba215ba19f35037b63d13655f
Basit <bta.aang@gmail.com>
2025-02-01 10:17:07 +0700

    tried saving data using userDefaults

--- GoDoIt/Controller/GoDoIt.swift
+++ GoDoIt/Controller/GoDoIt.swift
@@ -8,63 +8,76 @@
 import UIKit
 
 class GoDoIt: UITableViewController{
-    
-    var hardCodedData = ["Go Read A Book", "Go Jogging", "Go Buy Groceries"]
     var defaults = UserDefaults() //user default lari ke file plist, default bersifat persistent tapi cocoknya unntuk data yang kecil
+    var activityModel = Activity()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Panggil data dari userDefaults sebagai String
-        if let defaultData = defaults.array(forKey: "GoDoIt") as? [String]{
-            hardCodedData = defaultData
+        // Panggil data dari userDefaults sebagai Array of Dictionary
+        if let defaultData = defaults.array(forKey: "GoDoIt") as? [[String:Any]]{
+            activityModel.activities = defaultData
         }
     }
     
-    //MARK: - DATA SOURCES (Tempat Data Berasal)
+    //MARK: - DATA SOURCES (Tempat Data Berasal) - NumberOfRowInSection
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return hardCodedData.count
+        return activityModel.activities.count //jumlah activity di model
     }
     
+    //MARK: - DATA SOURCES (Tempat Data Berasal) - CellForRowAt
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Deklarasi reusable cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "GoDoItCell", for: indexPath)
-        cell.textLabel?.text = hardCodedData[indexPath.row]
+        
+        // Deklarasi text dan checkmark (khusus deklarasi aja, tanpa ubah nilai variabel
+        let textLabelForCell = activityModel.activities[indexPath.row]["name"] as? String
+        let checkmarkForCell = activityModel.activities[indexPath.row]["isDone"] as? Bool
+        cell.textLabel?.text = textLabelForCell
+        
+        // Ternary Operator in Swift
+        if let checkmark = checkmarkForCell{
+            cell.accessoryType = checkmark ? .checkmark : .none
+        }
         return cell
     }
     
-    //MARK: - DATA DELEGATE (Tempat Data Diolah/Digunakan)
+    //MARK: - DATA DELEGATE (Tempat Data Diolah/Digunakan) - DidSelectRowAt
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        if let act = activityModel.activities[indexPath.row]["isDone"] as? Bool {
+            activityModel.activities[indexPath.row]["isDone"] = !act
         }
+        tableView.reloadData() //reload TableView Cell (cellForRowAt)
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     //MARK: - TOMBOL TAMBAH DATA (Alert)
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        print("abc")
-        var extendedTextField = UITextField() //deklarasi textField utk menampung textField alert
+        var extendedTextField = UITextField() //deklarasi textField utk menampung alert textField
         
-        //Setting Alert
+        // Setting Alert
         let myAlert = UIAlertController(title: "Add Your Item", message: "Write Your Activity here!", preferredStyle: .alert)
         let myAction = UIAlertAction(title: "Add", style: .default) { (myAction) in
             print("success!!")
+            // Give value to activity model
             if let text = extendedTextField.text{
-                self.hardCodedData.append(text)
-                self.defaults.set(self.hardCodedData, forKey: "GoDoIt")
+                self.activityModel.activities.append([
+                    "name":text,
+                    "isDone":false
+                ])
+                // Store data to user defaults
+                self.defaults.set(self.activityModel.activities, forKey: "GoDoIt")
                 self.tableView.reloadData()
             }
         }
         
-        //Tambah Action Alert dan TextField
+        // Tambah Action Alert dan TextField
         myAlert.addAction(myAction)
         myAlert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Write your item here!"
             extendedTextField = alertTextField
         }
         
-        //Tampilkan/Jalankan Alert
+        // Tampilkan/Jalankan Alert
         present(myAlert, animated: true, completion: nil)
     }
 }
--- GoDoIt/Model/Activity.swift
+++ GoDoIt/Model/Activity.swift
@@ -5,4 +5,19 @@
 //  Created by Basit Tri Anggoro on 30/01/25.
 //
 
-import Foundation
+struct Activity {
+    var activities = [
+        [
+            "name":"Go Plant",
+            "isDone":true
+        ],
+        [
+            "name":"Go Gym",
+            "isDone":false
+        ],
+        [
+            "name":"Go Run",
+            "isDone":false
+        ],
+    ]
+}
